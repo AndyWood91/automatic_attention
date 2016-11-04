d804bc2273ab630789b15b949792c321fcd3dc15
AndyWood <awoo5332@gmail.com>
2016-11-04 11:32:38 +1100

    rewrote kb, works - need to add back into MainProc

--- program/automatic_attention.m
+++ program/automatic_attention.m
@@ -12,4 +12,10 @@ DATA = get_details('automatic_attention', {}, 1, false);
 [main_window, off_window, screen_dimensions] = PTB_screens(white, black);  % setup PTB screens
 create_gabors;
 
+for a = 1:11
+    instruction_slides{a} = ['Instructions/Slide', int2str(a), '.jpg'];
+end
+
+show_instructions(main_window, off_window, instruction_slides);
+
 MainProc(main_window, off_window, screen_dimensions, DATA);
--- program/functions/show_instructions.m
+++ program/functions/show_instructions.m
@@ -1,18 +1,15 @@
 function [] = show_instructions(main_window, off_window, instructions)
     
     % might want to set background and text colours as inputs
-    wait_time = 2;  % this should be an input
+%     wait_time = 2;  % this should be an input
 
     for a = 1:numel(instructions)
         
-%         Screen('FillRect', off_window, [255 255 255])  % clear offscreen window
-%         DrawFormattedText(off_window, instructions{a});  % write instructions to offscreen window
-%         instruction = Screen('MakeTexture', off_window, double(imread('Instructions/Slide1.jpg')));
-%         Screen('DrawTexture', off_window, instruction);
-%         Screen('DrawTexture', main_window, off_window, [], [5 5 600 600]);  % draw offscreen window on main window
-        Screen('MakeTexture', main_window, 
+        RestrictKeysForKbCheck(KbName('space'));
+        instruction_stimulus(a) = Screen('MakeTexture', main_window, imread(instructions{a}));
+        Screen('DrawTexture', main_window, instruction_stimulus(a), [], [0 0 1080 675]);
         Screen('Flip', main_window);  % refresh main window with new instructions
-        WaitSecs(wait_time);
+        accKbWait;
         
     end
 
